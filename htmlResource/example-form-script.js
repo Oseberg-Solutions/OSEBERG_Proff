6a2c57d29aec24d02f853272b57e3b16a8f6e4c8
sub <suranbasharati@gmail.com>
2024-03-01 17:12:43 +0100

    now we can fetch and set the values of ratings

--- htmlResource/example-form-script.js
+++ htmlResource/example-form-script.js
@@ -2,22 +2,39 @@ var Example = window.Example || {};
 
 Example.formOnLoad = function (executionContext) {
   console.log("Form on load!!!");
-
-  var formContext = executionContext.getFormContext();
-
-  var webResourceControl = formContext.getControl("WebResource_new_1");
-
-  if (webResourceControl) {
-    var data = {
-      orgNr: formContext.getAttribute("cr41c_orgnr").getValue(),
-      name: formContext.getAttribute("name").getValue(),
-    };
-
-    //fetchApiAndSetValues(data.orgNr);
-  }
+  setTimeout(function () {
+    console.log("Form on load2!!!");
+    Start(executionContext);
+  }, 5000);
 };
 
 window.Example = Example;
+
+function Start(executionContext) {
+  console.log("!!!!START!!!");
+  var formContext = executionContext.getFormContext();
+
+  //var webResourceControl = formContext.getControl("WebResource_new_1");
+
+  /*
+  var data = {
+    orgNr: formContext.getAttribute("cr41c_orgnr").getValue(),
+    name: formContext.getAttribute("name").getValue(),
+    score: "C++",
+    scorePoint: 34,
+  };
+  */
+
+  var webResourceIframe = document.getElementById("WebResource_new_1"); // The ID of your iframe
+  if (webResourceIframe) {
+    var targetOrigin = window.location.origin; // Replace this with the origin of your web resource
+    var message = {
+      action: "update",
+      data: { score: "D++", scorePoint: 34 },
+    };
+    webResourceIframe.contentWindow.postMessage(message, targetOrigin);
+  }
+}
 
 function fetchApiAndSetValues(orgNr) {
   const baseURL = "azure.com";
@@ -40,24 +57,35 @@ function fetchApiAndSetValues(orgNr) {
 </div>
 */
 
-function updateScoreAndCircle(iframe, data) {
+function updateScoreAndCircle(data) {
+  console.log("Update Score and Circle: ", data);
   var iframe = document.querySelector(
     'iframe[src*="/webresources/os_proff_rating"]'
   );
-  document.getElementById("score").innerHTML = data.score;
-  document.getElementById("scorePoint").innerHTML = data.scorePoint;
-  document.getElementById("ratingScore").innerHTML = data.scorePoint;
-  document.getElementById("scoreText").innerHTML = data.scorePoint;
-  document.getElementById("scoreMaxPoint").innerHTML = data.scorePoint;
+
+  console.log("IFrame: ", iframe.contentWindow.document.body);
+
+  iframe.contentWindow.document.getElementById("score").innerHTML = data.score;
+  iframe.contentWindow.document.getElementById("scorePoint").innerHTML =
+    data.scorePoint;
+  iframe.contentWindow.document.getElementById("ratingScore").innerHTML =
+    data.scorePoint;
+
   moveCircle(data.scorePoint);
 }
 
-function moveCircle(score) {
-  // Your existing moveCircle function
-}
+function updateCircleBorderColor(iframe, circle, leftPosition) {
+  var gaugeSections =
+    iframe.contentWindow.document.querySelectorAll(".gauge-section");
+  var colors = ["#ff6666", "#fbb72a", "#4bae49"]; // Colors corresponding to sections
+  var sectionWidth = 100 / gaugeSections.length; // Assuming equal width sections
 
-function updateCircleBorderColor(circle, leftPosition) {
-  // Your existing updateCircleBorderColor function
+  var sectionIndex = Math.min(
+    gaugeSections.length - 1,
+    Math.floor(leftPosition / sectionWidth)
+  );
+
+  circle.style.borderColor = colors[sectionIndex];
 }
 
 // You might not need the example call to moveCircle here,
